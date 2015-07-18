422c0072b8a214400798efec21f58b35ad6e9b80
Tyler Walker <twalker@eecs.wsu.edu>
2015-07-17 18:05:15 -0700

    Footnotes removed support of :general, caused hangs and errors

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -1,4 +1,4 @@
 if defined?(Footnotes) && Rails.env.development?
   Footnotes.run!
-  Footnotes::Filter.notes = [:session, :cookies, :params, :filters, :routes, :env, :queries, :log, :general]
+  Footnotes::Filter.notes = [:session, :cookies, :params, :filters, :routes, :env, :queries, :log]
 end
