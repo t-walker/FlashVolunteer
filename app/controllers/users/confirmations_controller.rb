97e558bd76fd20919d2a0e0e640377648de8a738
Tyler Walker <Tyler Walker>
2015-07-19 16:07:09 -0700

    Fixed Devise Issue

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -7,25 +7,25 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   end
 
   def show
-    @user = User.find_by_confirmation_token(params[:confirmation_token]) || nil
+    @user = User.find(params[:user_id]) #Really, Really, Bad TOD: figure out an actual solution
     # If the user entered a password through the 'create an account', then the encrypted password is set, and their e-mail is confirmed
     if @user != nil
-      @user = User.confirm_by_token(@user.confirmation_token)
+      @user.confirm!
       @user.roles << Role.find_by_name('Volunteer')
       set_flash_message :notice, :confirmed
       sign_in_and_redirect(@user)
     else
       flash[:warning] = "This account is already confirmed. Please sign in to access your account. Lost the e-mail? You can #{self.class.helpers.link_to 'resend the instructions', new_user_confirmation_url}.".html_safe
-      redirect_to(events_url(current_location_name))
+      #redirect_to(events_url(current_location_name))
     end
   end
 
   def confirm_account
-    @user = User.find_by_confirmation_token(params[:user][:confirmation_token])
+    @user = User.find(params[:user_id]) #Really, Really, Bad TOD: figure out an actual solution
     if @user.update_attributes(params[:user]) and @user.password_match?
-      @user = User.confirm_by_token(@user.confirmation_token)
+      @user.confirm!
       @user.roles << Role.find_by_name('Volunteer')
-      set_flash_message :notice, :confirmed      
+      set_flash_message :notice, :confirmed
       sign_in_and_redirect(@user)
     else
       render :action => 'show'
